3caf9104c5160bca9ff511b59fd7f926d329b05a
jasmaa <jasonmaa3955@gmail.com>
2021-01-07 21:59:48 -0500

    Truncate header image and content file paths

--- v1/serializers.py
+++ v1/serializers.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from core.models import *
@@ -31,8 +32,8 @@ class CategorySerializer(serializers.ModelSerializer):
 class ArticleSerializer(serializers.ModelSerializer):
     """Article serializer
     """
-    contentsFileName = serializers.ImageField(source='contents_file')
-    headerImageFileName = serializers.ImageField(source='header_image')
+    contentsFileName = serializers.SerializerMethodField('get_contents_path')
+    headerImageFileName = serializers.SerializerMethodField('get_header_image_path')
     dateCreated = serializers.DateTimeField(source='date_created')
     dateEdited = serializers.DateTimeField(source='date_edited')
     dateVisible = serializers.DateTimeField(source='date_created')
@@ -42,6 +43,14 @@ class ArticleSerializer(serializers.ModelSerializer):
 
     def get_full_name(self, article):
         return f'{article.author.first_name} {article.author.last_name}'.strip()
+
+    def get_contents_path(self, article):
+        p = Path(article.contents_file.url)
+        return '/'.join(p.parts[3:])
+
+    def get_header_image_path(self, article):
+        p = Path(article.header_image.url)
+        return '/'.join(p.parts[3:])
 
     class Meta:
         model = Article
